f0cfad92bb89b50176a61e5cccde8cbfb5578bb8
Sakshi <sakshigome1809@gmail.com>
2026-01-07 22:35:45 +0530

    Basic Patterns in Java

---
+++ Patterns/InvertedHalfPyramid.java
@@ -0,0 +1,14 @@
+package Patterns;
+
+public class InvertedHalfPyramid {
+	public static void main(String[] args) {
+		int n=5;
+		int m=5;
+		for(int i=n; i>=1; i--) {
+			for(int j=i; j>=1; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
