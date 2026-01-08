2b26a96cef7cc00a7471fae506a70a56d462cc52
Sakshi <sakshigome1809@gmail.com>
2026-01-08 10:13:35 +0530

    Area of Circle

---
+++ VariablesAndDataTypes/AreaOfCircle.java
@@ -0,0 +1,13 @@
+package VariablesAndDataTypes;
+
+public class AreaOfCircle {
+
+	public static void main(String[] args) {
+		double PI = 3.14;
+		int r = 7;
+		
+		double Area = PI*r*r;
+		
+		System.out.println("Area of Circle= "+Area);
+	}
+}
