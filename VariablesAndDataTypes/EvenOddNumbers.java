cf744eee85330ab0fb161f389c52eb11e005159e
Sakshi <sakshigome1809@gmail.com>
2026-01-08 10:17:01 +0530

    Even or Odd number

---
+++ VariablesAndDataTypes/EvenOddNumbers.java
@@ -0,0 +1,13 @@
+package VariablesAndDataTypes;
+
+public class EvenOddNumbers {
+	public static void main(String[] args) {
+		int num = 45;
+		
+		if(num%2==0) {
+			System.out.println(num+" is even number");
+		}else {
+			System.out.println(num+" is odd number");
+		}
+	}
+}
