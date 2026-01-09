d4d78d1ce375e34b368231384e65c4f85b6e7341
Sakshi <sakshigome1809@gmail.com>
2026-01-09 19:11:45 +0530

    Inverted and rotated half pyramid

--- Patterns/InvertedRotatedHalfPyramid.java
+++ Patterns/InvertedRotatedHalfPyramid.java
@@ -4,7 +4,8 @@ public class InvertedRotatedHalfPyramid {
 
 	public static void main(String[] args){
 		int n=5;
-		for(int i=1; i<=n; i++) {
+		
+		for(int i=n; i>=1; i--) {
 			for(int j=1; j<=n-i; j++) {
 				System.out.print(" ");
 			}
