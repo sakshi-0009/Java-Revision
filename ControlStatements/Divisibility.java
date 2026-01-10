329bdec8b81609791e3d8167858c3594023e5fb0
Sakshi <sakshigome1809@gmail.com>
2026-01-10 17:07:06 +0530

    Divisibility by 5 and 11

---
+++ ControlStatements/Divisibility.java
@@ -0,0 +1,20 @@
+package ControlStatements;
+
+import java.util.Scanner;
+
+public class Divisibility {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter number: ");
+		int num = sc.nextInt();
+		
+		if(num%5==0 && num%11==0) {
+			System.out.println(num+" is divisible by 5 and 11");
+		}else {
+			System.out.println(num+" is not divisible by 5 and 11");
+		}
+		sc.close();
+	}
+}
