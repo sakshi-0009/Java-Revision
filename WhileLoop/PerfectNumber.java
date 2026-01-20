99bfca708b09fd2ed4ce0afffb7c1ed5a693c0b2
Sakshi <sakshigome1809@gmail.com>
2026-01-20 17:42:25 +0530

    Perfect number

---
+++ WhileLoop/PerfectNumber.java
@@ -0,0 +1,27 @@
+package WhileLoop;
+
+import java.util.Scanner;
+
+public class PerfectNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter number: ");
+		int num = sc.nextInt();
+		
+		int sum = 0;
+		int i = 1;
+		while(i<=num/2) {
+			if(num%i==0) {
+				sum = sum+i;
+			}
+			i++;
+		}
+		if(sum==num && sum!=0) {
+			System.out.println(num+" is a perfect number");
+		}else {
+			System.out.println(num+" is not a perfect number");
+		}
+	}
+}
