2993b38909f4f460e6d25734f12509e50034fec4
Sakshi <sakshigome1809@gmail.com>
2026-01-23 17:37:13 +0530

    Reverse number using function

---
+++ Methods/ReverseNumber.java
@@ -0,0 +1,28 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+	static int reverseNumber(int num) {
+		
+		int rev = 0;
+		
+		while(num!=0) {
+			rev = rev*10+num%10;
+			num = num/10;
+		}
+		return rev;
+		
+	}
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number: ");
+		
+		int num = sc.nextInt();
+		int rev = reverseNumber(num);
+				
+		System.out.println("Reversed number is: "+rev);
+	}
+}
