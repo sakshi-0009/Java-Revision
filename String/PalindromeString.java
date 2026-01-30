b6e89b3de983a1e64a97985fcd10ca82bd7035a7
Sakshi <sakshigome1809@gmail.com>
2026-01-30 15:57:38 +0530

    Palindrome string

---
+++ String/PalindromeString.java
@@ -0,0 +1,25 @@
+package String;
+
+import java.util.Scanner;
+
+public class PalindromeString {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter string: ");
+		String str = sc.next();
+		
+		String rev = "";
+		
+		for(int i=str.length() - 1; i>=0; i--) {
+			rev = rev+str.charAt(i);
+		}
+		
+		if(str.equals(rev)) {
+			System.out.println("Palindrome String!");
+		}else {
+			System.out.println("Not a palindrome string");
+		}
+	}
+}
