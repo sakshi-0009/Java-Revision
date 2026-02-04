13d2b6e3a218b3e094b728ab23ff893800aca8bc
Sakshi <sakshigome1809@gmail.com>
2026-02-04 20:24:30 +0530

    Fetching account details

--- classesObjects/BankAccount.java
+++ classesObjects/BankAccount.java
@@ -2,4 +2,26 @@ package classesObjects;
 
 public class BankAccount {
 
+	int accountNo;
+	String name;
+	double balance;
+	
+	BankAccount(){
+		System.out.println("Account details: ");
+	}
+	void display() {
+		System.out.println("Account number: "+accountNo);
+		System.out.println("Account holder name: "+name);
+		System.out.println("Balance: "+balance);
+	}
+	
+	public static void main(String[] args) {
+		
+		BankAccount acc = new BankAccount();
+		acc.accountNo = 101;
+		acc.name = "Sakshi";
+		acc.balance = 101000;
+		
+		acc.display();	
+	}
 }
