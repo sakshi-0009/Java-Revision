f9f193bfc79543504a34c48df3cb5d9a10523453
Sakshi <sakshigome1809@gmail.com>
2026-02-07 22:12:20 +0530

    Fetching exam details

--- classesObjects/TETExam.java
+++ classesObjects/TETExam.java
@@ -4,13 +4,16 @@ public class TETExam {
 
 	String exam_name = "TET";
 	int candidates = 25000;
+	int passed_candidates = 3000;
 	
 	TETExam(){
 		System.out.println("Exam details: ");
 	}
+	
 	void Exam(){
 		System.out.println("Exam name: "+exam_name);
 		System.out.println("Appeared candidates: "+candidates);
+		System.out.println("Passed candidates: "+passed_candidates);
 	}
 	
 	public static void main(String[] args) {
