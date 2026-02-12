cec12979dc4b4c4315fda356fc3e399ed4af3580
Sakshi <sakshigome1809@gmail.com>
2026-02-12 12:02:31 +0530

    Bank account details using enapsulation

--- Encapsulation/Main.java
+++ Encapsulation/Main.java
@@ -3,11 +3,11 @@ package Encapsulation;
 public class Main {
 
 	public static void main(String[] args) {
-		BankAccount ac = new BankAccount(5000);
+		BankAccount acc = new BankAccount(5000);
 		
-		ac.deposit(1000);
-		ac.withdraw(3000);
+		acc.deposit(1000);
+		acc.withdraw(3000);
 		
-		System.out.println("Balance: "+ac.getBalance());
+		System.out.println("Balance: "+acc.getBalance());
 	}
 }
