1dd5d6c6589baa1efaf306c7600df8add11dc1c4
Sakshi <sakshigome1809@gmail.com>
2026-02-16 17:51:07 +0530

    Example of Inheritance

---
+++ Inheritance/Client1.java
@@ -0,0 +1,35 @@
+package Inheritance;
+
+class BCCI{
+	
+	BCCI(){
+		System.out.println("In BCCI Constructor");
+	}
+	
+	void BCCIDemo(){
+		System.out.println("Board of Control for Cricket in India");
+	}
+}
+
+class IPL extends BCCI {
+	
+	IPL(){
+		System.out.println("In IPL Constructor");
+	}
+	
+	void IPLDemo() {
+		System.out.println("Indian Premier League");
+	}
+}
+
+class Client1 {
+	
+	public static void main(String[] args) {
+		BCCI obj = new BCCI();
+		IPL obj1 = new IPL();
+		BCCI obj2 = new IPL();
+		
+		obj.BCCIDemo();
+		obj1.IPLDemo();
+	}
+}
