5b48809d93f7e4f9dccac1c601cfd1d1b0ab6007
Sakshi <sakshigome1809@gmail.com>
2026-02-17 15:58:36 +0530

    Example of Inheritance

---
+++ Inheritance/Client2.java
@@ -0,0 +1,31 @@
+package Inheritance;
+
+class Employee{
+	
+	int salary = 30000;
+	
+	void work() {
+		System.out.println("Employee is working!");
+	}
+}
+
+class Manager extends Employee{
+	
+	int bonus = 10000;
+	
+	void manage() {
+		System.out.println("Manager is managing team.");
+	}
+}
+
+class Client2 {
+
+	public static void main(String[] args) {
+		
+		Manager m = new Manager();
+		System.out.println("Total Salary: "+(m.salary+m.bonus));
+		
+		m.work();
+		m.manage();
+	}
+}
