4ee070a49c708efff87761b0748cf65f15a8908d
Sakshi <sakshigome1809@gmail.com>
2026-02-22 16:02:05 +0530

    Example of method overriding

---
+++ Polymorphism/OverridingDemo.java
@@ -0,0 +1,30 @@
+package Polymorphism;
+
+class Parent{
+	
+	Parent(){
+		System.out.println("In Parent Constructor");
+	}
+	void fun(int x) {
+		System.out.println("In parent fun method");
+	}
+}
+
+class Child extends Parent{
+	
+	Child(){
+		System.out.println("In Child Constructor");
+	}
+	void fun() {
+		System.out.println("In child fun method");
+	}
+}
+
+class OverridingDemo {
+
+	public static void main(String[] args) {
+		
+		Parent obj = new Child();
+		obj.fun(10);
+	}
+}
